daaf43a4660af9bb03f6f808011bd4b411e1d960
MBB <pynchia@gmail.com>
2015-05-13 12:28:29 +0200

    now able to search entries  on list action

--- api/views.py
+++ api/views.py
@@ -8,12 +8,19 @@ class EntryViewSet(viewsets.ModelViewSet):
     """
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
+    The 'list' action allows searching (case ins.) for a substring
+    of the long URL via the url param. (e.g. /api/entries/?url=<substring>)
     """
     queryset = Entry.objects.all()
     serializer_class = serializers.EntrySerializer
 
+    def get_queryset(self):
+        substring = self.request.QUERY_PARAMS.get('url', None)
+        if substring is not None:
+            return self.queryset.filter(url__contains=substring.lower())
+        return self.queryset
+
     def get_serializer_class(self):
         if self.action == 'list':
             return serializers.EntryListSerializer
-        else:
-            return super(EntryViewSet, self).get_serializer_class()
+        return super(EntryViewSet, self).get_serializer_class()
